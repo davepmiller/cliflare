d25fcbf347c19d8bb74bf92800438fd6aaf0da4a
Dave Miller <dave@optimism.com>
2024-01-07 23:19:33 -0700

    add token happy path unit test

--- src/token.rs
+++ src/token.rs
@@ -10,3 +10,18 @@ impl Token {
         println!("{response:?}");
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::client;
+    use crate::token::{Token, PATH};
+
+    #[test]
+    fn verify() {
+        let mock = client::tests::build_server_mock("GET", format!("/{}", PATH).as_str())
+            .with_body(client::tests::response_body())
+            .create();
+        Token::verify();
+        mock.assert();
+    }
+}
